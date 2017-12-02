063709856e95226aa628fb9d8cdc2c5de9a5521a
huangchangxu <changxu.huang@ds365.com>
2017-12-02 12:09:26 +0800

    autofac   中间件

---
+++ PaymentCenter.Infrastructure/AutofacConfig/DefaultModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentCenter.Infrastructure.AutofacConfig
+{
+    public class DefaultModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //注册服务
+        }
+    }
+}
